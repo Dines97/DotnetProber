ec76d50b68b5f2bc3fbdffb4f58eb5c206ffc9e8
Denis Kaynar <kaynar.denis@gmail.com>
2023-04-13 14:08:30 +0300

    feat: generate.sh added

---
+++ Prober/generate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+dotnet run -- generator docker --solution-dir .. --project-path ./Prober.csproj --target-file Prober.dll --dotnet-tag 7.0 -o Dockerfile
+dotnet run -- generator crd -o ./config/crds
+dotnet run -- generator installer -o ./config/installer
+dotnet run -- generator operator -o ./config/operator
+dotnet run -- generator rbac -o ./config/rbac
